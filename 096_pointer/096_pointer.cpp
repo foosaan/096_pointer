e30225468166e599a25d49efb960db5d0c5fe24f
foosaan <fauzan4297@gmail.com>
2023-05-10 11:37:05 +0700

    membuat pointer

--- 096_pointer/096_pointer.cpp
+++ 096_pointer/096_pointer.cpp
@@ -24,5 +24,5 @@ int main()
 	pMhs->nim = 3;			//arrow operator
 	pMhs->showNim();
 	system("pause");
-	return 0;
+	return 0;
 }
--- friendclass2/friendclass2.cpp
+++ friendclass2/friendclass2.cpp
@@ -1,20 +1,41 @@
-// friendclass2.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+using namespace std;
+class siswa;
+class orang {
+private:
+	string nama;
+public:
+	void setNama(string pNama);
+	friend class siswa;
+};
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class siswa {
+private:
+	int id;
+public:
+	void setId(int pId);
+	void displayAll(orang& a);
+};
+
+void siswa::displayAll(orang& a) {
+	cout << id << endl << a.nama;
+}
+void siswa::setId(int pId) {
+	id = pId;
+}
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+int main() {
+	orang o;
+	o.setNama("Joko Kumat");
+	siswa s;
+	s.setId(1);
+	s.displayAll(o);
+	cout << endl;
+	system("pause");
+	return 0;
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
