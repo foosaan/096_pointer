7953e0beaef24b60e8819507133b81987deef2d5
foosaan <fauzan4297@gmail.com>
2023-05-10 11:10:47 +0700

    memberi perintah new pada mahasiswa dan perintah delete

--- 096_pointer/friendclass.cpp
+++ 096_pointer/friendclass.cpp
@@ -5,8 +5,16 @@ class mahasiswa {
 public:
 	int nim;
 	void showNim() {
-		cout << "No Induk =" << mhs << endl;
+		cout << "No Induk =" << nim << endl;
 	}
 
 };
- int main()
+int main() {
+	mahasiswa* mhs = new mahasiswa{ 1 };
+	mhs->nim = 2;
+	mhs->showNim();
+	delete mhs;
+	system("pause");
+	return 0;
+}
+
